052df87ca6d49e954349a95034e42c3cda5691ab
Moh Moh San <violalwin@gmail.com>
2020-06-15 19:37:42 +0800

    Adding validation and test to have unique title for room

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,62 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: 'user@gmail.com',password:"user123!")
+
+
+Room.create(room_name: 'L1_M101', room_type: "Meeting Room",floor:1)
+Room.create(room_name: 'L1_M102', room_type: "Meeting Room",floor:1)
+Room.create(room_name: 'L1_M103', room_type: "Meeting Room",floor:1)
+Room.create(room_name: 'L1_M104', room_type: "Meeting Room",floor:1)
+Room.create(room_name: 'L1_M105', room_type: "Meeting Room",floor:1)
+Room.create(room_name: 'L1_M106', room_type: "Meeting Room",floor:1)
+Room.create(room_name: 'L1_M107', room_type: "Meeting Room",floor:1)
+Room.create(room_name: 'L1_M108', room_type: "Meeting Room",floor:1)
+
+Room.create(room_name: 'L1_F101', room_type: "Facility Room",floor:1)
+Room.create(room_name: 'L1_F102', room_type: "Facility Room",floor:1)
+Room.create(room_name: 'L1_F103', room_type: "Facility Room",floor:1)
+Room.create(room_name: 'L1_F104', room_type: "Facility Room",floor:1)
+Room.create(room_name: 'L1_F105', room_type: "Facility Room",floor:1)
+Room.create(room_name: 'L1_F106', room_type: "Facility Room",floor:1)
+Room.create(room_name: 'L1_F107', room_type: "Facility Room",floor:1)
+Room.create(room_name: 'L1_F108', room_type: "Facility Room",floor:1)
+
+
+Room.create(room_name: 'L2_M201', room_type: "Meeting Room",floor:2)
+Room.create(room_name: 'L2_M202', room_type: "Meeting Room",floor:2)
+Room.create(room_name: 'L2_M203', room_type: "Meeting Room",floor:2)
+Room.create(room_name: 'L2_M204', room_type: "Meeting Room",floor:2)
+Room.create(room_name: 'L2_M205', room_type: "Meeting Room",floor:2)
+Room.create(room_name: 'L2_M206', room_type: "Meeting Room",floor:2)
+Room.create(room_name: 'L2_M207', room_type: "Meeting Room",floor:2)
+Room.create(room_name: 'L2_M208', room_type: "Meeting Room",floor:2)
+
+Room.create(room_name: 'L2_F201', room_type: "Facility Room",floor:2)
+Room.create(room_name: 'L2_F202', room_type: "Facility Room",floor:2)
+Room.create(room_name: 'L2_F203', room_type: "Facility Room",floor:2)
+Room.create(room_name: 'L2_F204', room_type: "Facility Room",floor:2)
+Room.create(room_name: 'L2_F205', room_type: "Facility Room",floor:2)
+Room.create(room_name: 'L2_F206', room_type: "Facility Room",floor:2)
+Room.create(room_name: 'L2_F207', room_type: "Facility Room",floor:2)
+Room.create(room_name: 'L2_F208', room_type: "Facility Room",floor:2)
+
+
+Room.create(room_name: 'L3_M301', room_type: "Meeting Room",floor:3)
+Room.create(room_name: 'L3_M302', room_type: "Meeting Room",floor:3)
+Room.create(room_name: 'L3_M303', room_type: "Meeting Room",floor:3)
+Room.create(room_name: 'L3_M304', room_type: "Meeting Room",floor:3)
+Room.create(room_name: 'L3_M305', room_type: "Meeting Room",floor:3)
+Room.create(room_name: 'L3_M306', room_type: "Meeting Room",floor:3)
+Room.create(room_name: 'L3_M307', room_type: "Meeting Room",floor:3)
+Room.create(room_name: 'L3_M308', room_type: "Meeting Room",floor:3)
+
+Room.create(room_name: 'L3_F301', room_type: "Facility Room",floor:3)
+Room.create(room_name: 'L3_F302', room_type: "Facility Room",floor:3)
+Room.create(room_name: 'L3_F303', room_type: "Facility Room",floor:3)
+Room.create(room_name: 'L3_F304', room_type: "Facility Room",floor:3)
+Room.create(room_name: 'L3_F305', room_type: "Facility Room",floor:3)
+Room.create(room_name: 'L3_F306', room_type: "Facility Room",floor:3)
+Room.create(room_name: 'L3_F307', room_type: "Facility Room",floor:3)
+Room.create(room_name: 'L3_F308', room_type: "Facility Room",floor:3)
