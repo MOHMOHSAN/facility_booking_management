f0f756f855f13f041a3b1ae36f1d8e9b2b0f3bce
Moh Moh San <violalwin@gmail.com>
2020-06-17 03:09:56 +0800

    adding association between model
    1. 1 Room has many booking
    2. 1 User has many booking

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,6 @@ class Room < ApplicationRecord
 	validates_presence_of :floor
 	validates_presence_of :room_type
 	validates_uniqueness_of :room_name
+	has_many :bookings, dependent: :destroy, inverse_of: :room
+	
 end
