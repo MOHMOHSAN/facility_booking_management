f6e908979950e7229322ce1411884ec4b287cfc0
Moh Moh San <violalwin@gmail.com>
2020-06-19 13:48:05 +0800

    limit the use of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  	resources :bookings
+  	resources :bookings, only: [:index,:create,:destroy]
   	devise_for :users
 	root 'rooms#search'
 
-	resources :rooms
+	resources :rooms, only: [:index]
 
 	get 'rooms/search' => 'rooms#search'
 	match "rooms/data", :to => "rooms#data", :via => "post"
